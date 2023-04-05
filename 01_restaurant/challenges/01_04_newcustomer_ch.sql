86751ac94168ed9f87efbcfe8cd12dd21b185421
Maria Bogdanas <87804886+mariabogdanas@users.noreply.github.com>
2023-04-05 14:40:26 +0000

    solution

--- 01_restaurant/challenges/01_04_newcustomer_ch.sql
+++ 01_restaurant/challenges/01_04_newcustomer_ch.sql
@@ -4,3 +4,17 @@
 -- Anna Smith (asmith@samoca.org)
 -- 479 Lapis Dr., Memphis, TN
 -- Phone: (555) 555-1212; Birthday: July 21, 1973
+INSERT INTO Customers (CustomerID, 
+            FirstName, LastName, Email,
+            Address,City,State,Phone,Birthday)
+values ((select max(CustomerID) + 1
+           from Customers),
+        "Anna",
+        "Smith", 
+        "asmith@samoca.org",
+        "479 Lapis Dr.",
+        "Memphis",
+        "TN",
+        "555-555-1212",
+        "1973-07-21");
+select * from Customers;
--- 01_restaurant/challenges/01_05_updateinfo_ch.sql
+++ 01_restaurant/challenges/01_05_updateinfo_ch.sql
@@ -2,3 +2,13 @@
 
 -- Taylor Jenkins, of 27170 6th Ave., Washington, DC,
 -- has moved to 74 Pine St., New York, NY.
+UPDATE Customers
+   set Address = '74 Pine St.',
+       City = 'New York',
+       State = 'NY'
+ WHERE FirstName = 'Taylor' 
+   and LastName = 'Jenkins'
+   and Address = '27170 6th Ave.';
+
+select * from Customers;
+      
